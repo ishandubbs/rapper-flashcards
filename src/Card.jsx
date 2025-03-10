6ce6eda830e6915aa22db7f6cdf9666c68d154cc
Ishan Roy <iroy3@student.gsu.edu>
2025-03-10 10:53:14 -0400

    Added additional variables and buttons for various functions

--- src/Card.jsx
+++ src/Card.jsx
@@ -26,12 +26,27 @@ function Card({ question, answer, img, difficulty, onNext, onPrevious, onMaster 
         <div className="card-container">
             <div className={`card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
                 <div className="card-inner">
+                    { /* FRONT */ }
                     <div className={`card-front ${difficulty || 'default'}`}>
                         <p>{question}</p>
+                        <input
+                            type="text"
+                            value={userInput}
+                            onChange={(e) => setUserInput(e.target.value)}
+                            placeholder="Your answer..."
+                        />
+                        <button onClick={handleSubmit}>Submit</button>
+                        <p className="feedback">{feedback}</p>
+                        <button onClick={handleFlip}>Flip</button>
                     </div>
+                    { /* BACK */ }
                     <div className={`card-back ${difficulty || 'default'}`}>
                         <p>{answer}</p>
-                        <img src = {img} alt = "Answer" className="card-img"/>
+                        {img && <img src = {img} alt = "Answer" className="card-img"/>}
+                        <button onClick={handleFlip}>Flip</button>
+                        <button onClick={onMaster}></button>
+                        <button onClick={onPrevious}></button>
+                        <button onClick={onNext}></button>
                     </div>
                 </div>
             </div>
